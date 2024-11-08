a7ec7022a095c2bf264fdeea3495977d03fd655f
samat <samhardy1927@gmail.com>
2024-11-08 16:14:45 +0300

    group reserved name by sql, changed to studentGroup

--- src/main/java/com/beganov/studentservice/model/Student.java
+++ src/main/java/com/beganov/studentservice/model/Student.java
@@ -1,18 +1,20 @@
 package com.beganov.studentservice.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
+@Table(name = "students")
 public class Student {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
     private String lastName;
     private String firstName;
     private String middleName;
-    private String group;
+    private String studentGroup;
     private Double averageGrade;
 }
