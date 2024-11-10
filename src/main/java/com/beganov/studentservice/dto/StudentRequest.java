9821474f1750d37d443ea54c055036d1ab96dcba
samat <samhardy1927@gmail.com>
2024-11-10 16:41:27 +0300

    StudentRequest and StudentResponse changed

--- src/main/java/com/beganov/studentservice/dto/StudentRequest.java
+++ src/main/java/com/beganov/studentservice/dto/StudentRequest.java
@@ -1,14 +1,33 @@
 package com.beganov.studentservice.dto;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.math.BigDecimal;
 
 @Getter
 @Setter
 public class StudentRequest {
+
+    @NotNull
+    @Size(max = 50)
     private String firstName;
+
+    @NotNull
+    @Size(max = 50)
     private String lastName;
+
     private String middleName;
+
+    @NotNull
+    @Size(max = 50)
     private String studentGroup;
-    private Double averageGrade;
+
+    @Min(1)
+    @Max(5)
+    private BigDecimal averageGrade;
 }
--- src/main/java/com/beganov/studentservice/dto/StudentResponse.java
+++ src/main/java/com/beganov/studentservice/dto/StudentResponse.java
@@ -3,6 +3,8 @@ package com.beganov.studentservice.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.math.BigDecimal;
+
 @Getter
 @Setter
 public class StudentResponse {
@@ -11,5 +13,5 @@ public class StudentResponse {
     private String lastName;
     private String middleName;
     private String studentGroup;
-    private Double averageGrade;
+    private BigDecimal averageGrade;
 }
