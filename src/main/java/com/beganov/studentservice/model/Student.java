5f0403c6c3a8d811000f5683c0b4cd4832f9c2c2
samat <samhardy1927@gmail.com>
2024-11-10 16:39:13 +0300

    Changes in Student

--- src/main/java/com/beganov/studentservice/model/Student.java
+++ src/main/java/com/beganov/studentservice/model/Student.java
@@ -1,20 +1,40 @@
 package com.beganov.studentservice.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.math.BigDecimal;
 
 @Entity
 @Getter
 @Setter
 @Table(name = "students")
 public class Student {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotNull
+    @Column(nullable = false, length = 50)
     private String lastName;
+
+    @NotNull
+    @Column(nullable = false, length = 50)
     private String firstName;
+
     private String middleName;
+
+    @NotNull
+    @Column(nullable = false, length = 50)
     private String studentGroup;
-    private Double averageGrade;
+
+    @Min(1)
+    @Max(5)
+    @Column(precision = 2, scale = 1)
+    private BigDecimal averageGrade;
 }
